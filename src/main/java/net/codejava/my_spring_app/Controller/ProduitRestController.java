433ae03354beade3e729b6bad5beb9e1a1be0998
Marylene <marylene253925@gmail.com>
2026-08-24 09:13:27 +0200

    créer l'api permettant de consulter, creer , modifier, supprimer un produit | retourner tous les produits | retourner la, liste des produits ayant une catégorie donnée

--- src/main/java/net/codejava/my_spring_app/Controller/ProduitRestController.java
+++ src/main/java/net/codejava/my_spring_app/Controller/ProduitRestController.java
@@ -3,12 +3,24 @@ package net.codejava.my_spring_app.Controller;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import net.codejava.my_spring_app.Entities.Produit;
-import net.codejava.my_spring_app.Service.ProduitService;;
+import net.codejava.my_spring_app.Service.ProduitService;
+import org.springframework.web.bind.annotation.RequestMethod;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+
+
 
 @RestController // dis à spring que cette classe va contenir des methodes qui seront des web service qui seront consommé à travers un navigateur
 @RequestMapping("/api") // sert à définir une URL (une route) sur laquelle le Controller va répondre. C'est l'annotation la plus générale pour dire "cette classe ou cette méthode répond à telle adresse".
@@ -17,10 +29,45 @@ import net.codejava.my_spring_app.Service.ProduitService;;
 
 public class ProduitRestController {
 
-    private final ProduitService produitService = null;
+    private final ProduitService produitService;
     
-    @GetMapping() // equivalent de @RequestMapping(method=RequestMethod.GET)
-    List<Produit> getAllProduits(){
+
+    /* Récupére tous les produit */ 
+    //@GetMapping() // equivalent de @RequestMapping(method=RequestMethod.GET)
+     @RequestMapping(method=RequestMethod.GET)
+    public List<Produit> getAllProduits(){
         return produitService.getAllProduits();
     }
+
+    /* Récupère un produit par son id */
+    @GetMapping("/{id}")
+    public Produit getProduitById(@PathVariable("id") Long id){
+        return produitService.getProduit(id);
+    }
+
+    /* Création d'un produit */
+    @PostMapping()
+    public Produit createProduit(@RequestBody Produit produit){
+        return produitService.saveProduit(produit);
+    }
+
+    /* Modifier un produit */
+    @PutMapping()
+    public Produit modifieProduit(@RequestBody Produit produit){
+        return produitService.updateProduit(produit);
+    }
+
+    /* Supprimer un produit */
+    @DeleteMapping("/{id}")
+    public void deleProduit(@PathVariable("id") Long id){
+        produitService.deleteProduitById(id);
+    }
+
+    /* retorune produit ayant une catégorie données */
+    @GetMapping("/produitCat/{idCat}")
+    public List<Produit> getProduitsByCatId(@PathVariable("idCat") long idCat){
+        return produitService.findByCategorieIdCat(idCat);
+    }
+    
+
 }
